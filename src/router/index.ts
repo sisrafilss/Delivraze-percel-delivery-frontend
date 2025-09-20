c5f2fa9720d6841a95702ccd9939308028ed9589
Israfil Hossen <sisrafilss@gmail.com>
2025-09-20 15:40:43 +0600

    navbar footer and layout added

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import App from "@/App";
+import About from "@/pages/About";
+import HomePage from "@/pages/HomePage";
+import { createBrowserRouter } from "react-router";
+
+export const router = createBrowserRouter([
+  {
+    Component: App,
+    path: "/",
+    children: [
+      {
+        Component: HomePage,
+        index: true,
+      },
+      {
+        Component: About,
+        path: "about",
+      },
+    ],
+  },
+]);
