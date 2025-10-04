15acd9484213dcb2c58e3315cac5a4823737e283
Israfil Hossen <sisrafilss@gmail.com>
2025-10-04 21:21:17 +0600

    updated

--- src/components/modules/Authentication/LoginForm.tsx
+++ src/components/modules/Authentication/LoginForm.tsx
@@ -70,7 +70,9 @@ export function LoginForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="border-0 bg-none shadow-none">
         <CardHeader>
-          <CardTitle className="text-2xl">Login to your account</CardTitle>
+          <CardTitle className="text-2xl">
+            Login to your account | Modified
+          </CardTitle>
           <CardDescription>
             Enter your email and password below to login to your account
           </CardDescription>
